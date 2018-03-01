0042e7b482f39e4435fcd7422ad304a1d8ef169b
CianLR <cian.ruane1@gmail.com>
2018-03-01 17:42:59 +0000

    Add gen output script

---
+++ gen_output.py
@@ -0,0 +1,33 @@
+import sys
+import os
+import subprocess
+
+inputs = [
+
+]
+
+main_file = sys.argv[1]
+
+if not os.path.exists('output'):
+    os.makedirs('output')
+else:
+    subprocess.call(['rm output/*'], shell=True)
+
+source_paths = []
+for thing_in_dir in os.listdir('.'):
+    if thing_in_dir.endswith('.py'):
+        source_paths.append(thing_in_dir)
+
+print("Zipping:", ', '.join(source_paths))
+command = "zip output/source.zip " + ' '.join(source_paths)
+subprocess.call([command], shell=True)
+
+for in_file in inputs:
+    out_file = 'output/' + in_file.split('/')[1][:-2] + 'out'
+    command = ' '.join(["python3", main_file, "<", in_file, ">", out_file])
+    print("Running:", command)
+    return_code = subprocess.call([command], shell=True)
+    print(in_file, "return code:", return_code)
+    print("-------")
+
+
