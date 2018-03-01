2b223dc83d39d5e5c431a583876e891d180b60e0
CianLR <cian.ruane1@gmail.com>
2018-03-01 18:47:41 +0000

    Move Car around

--- util.py
+++ util.py
@@ -4,5 +4,26 @@ def man_dist(a, b):
     return abs(c.real) + abs(c.imag)
 
 
+class Car:
+    def __init__(self):
+        self.rides = []
+        self.current = 0 + 0j
+        self.ready = 0
+
+    def print(self):
+        print(len(self.rides), *[r.ride_id for r in self.rides])
+
+    def add_ride(self, r):
+        to_get = man_dist(self.current, r.cx_start)
+        wait_time = max(0, r.start_time - (self.ready + to_get))
+        self.ready += to_get + wait_time + r.ride_time
+        self.current = r.cx_end
+        self.rides.append(r)
+
+    def can_make_ride(self, r):
+        dst = man_dist(self.current, r.cx_start)
+        return self.ready + dst + r.ride_time <= r.finish_time
+
+
 
 
